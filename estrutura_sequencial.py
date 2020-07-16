58d6ba569288bef2e4821ba8f69425d72ccfc5f3
Bea Portella <blpportella@gmail.com>
2020-07-16 15:37:29 -0300

    Create estrutura_sequencial.py

---
+++ estrutura_sequencial.py
@@ -0,0 +1,56 @@
+#faça um programa que mostre a mensagem "Alo mundo" na tela
+
+print("Hallo Welt")
+
+#faça um programa que peça um numero e então mostre a mensagem "O numero informado foi [numero]
+
+numero_input = int(input("Coloque um número: "))
+print("O numero informado foi {}".format(numero_input))
+
+#Faça um Programa que peça dois números e imprima a soma.
+
+x = int(input("Escolha o primeiro número: "))
+y = int(input("Escolha o segundo número: "))
+
+def soma(x,y):
+    return x + y
+
+print(soma(x,y))
+
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+indice = 1
+nota_total = []
+
+while (indice <= 4):
+    nota_input = int(input("Coloque uma nota: "))
+    nota_total.append(nota_input)
+    indice = indice + 1
+
+def media_das_notas():
+    soma_das_notas = (sum(nota_total))
+    return soma_das_notas/4
+
+print(media_das_notas())
+
+#Faça um Programa que converta metros para centímetros.
+
+def metros_para_centimetros():
+    numero_em_metros = float(input("Coloque o valor em metros para ser convertido: "))
+    return numero_em_metros * 100
+
+print("{} cm".format(metros_para_centimetros()))
+
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input("Digite o raio para calculo da área do círculo: "))
+
+def area_circulo():
+    area = raio * raio * 3.14
+    return area
+
+print("A área do círculo é: {}".format(area_circulo()))
+
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+
